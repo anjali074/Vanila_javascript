ff7cc7eee9d62e388374636877792f27212bd863
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-06 15:44:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+//loop are used executed pices of code again $ again
+/*console.log("student");
+console.log("student");
+console.log("student");
+console.log("student");
+console.log("student");*/
+
+//esay way use in loop
+//1. for-loop{
+  
+/*for(let i=1; i<=5; i++){
+  console.log("student");
+}*/
+//print count 1 to n
+/*let count =1;
+for(count=1; count<=5; count ++){
+  count ++;
+  console.log("count");*/
+  //Q-calculate sum of first number 1 to n
+  /*let sum=0;
+  let n=100;
+  for(let i=1; i<=n; i++){
+    sum+=i;
+    console.log("sum=",sum);
+  }*/
+  //variable is not esxecute only for loop but use out the loop
+  /*for (var i=1;i<=5; i++){
+    console.log("i",i){
+
+    }
+    console.log(i);// print 1 to 5 than last 6
+  }*/
+
+//infinite loop
+//not run crash the window than full memory
+
+/*for(let i=1; i>=0; i++){
+  console.log("i",i);
+}*/
+//while-loop
+let i=1;
+while(i<=5){
+  console.log("i=",i);
+  i++;
+}
+//do-while loop
+let ii =20;
+do{
+  console.log("student");
+  i++;
+}
+  while(i<=10);
