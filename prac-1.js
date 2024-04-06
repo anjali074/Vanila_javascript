37d63234d59dae2822ac429b243d6feb3ac8b31f
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-06 16:10:02 +0530

    Add files via upload

---
+++ prac-1.js
@@ -0,0 +1,21 @@
+//q-print all even number from 0 to 100
+//print all number 0 to n
+/*let num=0;
+let n =100;
+for(let num=0; num<=100; num++){
+  console.log("num=",num);
+
+
+}*/
+for(let num=0; num<=100; num++){
+  if(num%2===0){//even number
+console.log("num=", num);
+
+  }
+}
+//odd number
+for(let num=0; num<=100; num++){
+  if(num%2!==0){//even number
+console.log("num=", num);
+  }
+}
