816bedb24b546d9ba2793b964ce33a7883fbc347
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-06 16:19:54 +0530

    Add files via upload

---
+++ prac-2.js
@@ -0,0 +1,8 @@
+//create a game whwre you strat with any random game number . Ask the user to keep guessing the game number until thr user enter correct value
+//pratice q-2
+let gameNum=25;
+let userNum=prompt("Guess the game number");
+while(userNum!==gameNum){
+  userNum=prompt(" wrongenter guess umberGuess the game number");
+}  
+console.log("Congrarulation , you eneterd the right number");
